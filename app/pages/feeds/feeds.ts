7085577de2e76ca8eafc32cb1f3497fe526e4d00
Muhammad-MuZzammil <Muhammadmuzzammil10@hotmail.com>
2016-06-03 20:02:17 +0500

    cordova plugin for camera implemented

--- app/pages/feeds/feeds.ts
+++ app/pages/feeds/feeds.ts
@@ -1,10 +1,12 @@
-import { Page } from "ionic-angular";
+import { Page , ActionSheet,NavController } from "ionic-angular";
 import { OnInit } from "@angular/core"
 import { FeedCardPage } from "../feedCard/feedCard";
 import { FeedModel } from "../feedCard/feedModel";
 import {FirebaseService} from '../../services/firebase';
 import {FirebaseListObservable} from 'angularfire2'
 import { Observable } from 'rxjs/Observable'
+import {Camera} from 'ionic-native'
+
 
 @Page({
     templateUrl: `build/pages/feeds/feeds.html`,
@@ -12,9 +14,11 @@ import { Observable } from 'rxjs/Observable'
 })
 export class FeedsPage implements OnInit {
 
+        image = "";
+
     feeds: Observable<any[]>;
 
-    constructor(private fs: FirebaseService) {
+    constructor(private fs: FirebaseService ,private nav:NavController) {
         
         this.feeds = this.fs.getData();
 
@@ -74,6 +78,65 @@ export class FeedsPage implements OnInit {
         //     }
         // ]
     }
+    
+    captureImage(){
+        
+        let imageFromCamera = false;
+        
+        let actionSheet = ActionSheet.create({
+            title: 'Select Method',
+            buttons:[
+                {
+                    text:'Camera',
+                    handler: () => {
+                        console.log('camera Clicked');
+                        imageFromCamera = true;
+                        getImage();
+                    }
+                },
+                 {
+        text: 'Gallery',
+        handler: () => {
+          console.log('Gallery clicked');
+          imageFromCamera = false;
+          getImage();
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }
+            ]
+        });
+        
+        this.nav.present(actionSheet)
+        
+        
+        let getImage= ()=>{
+            let options = {
+                destinationType: 0,
+                sourceType : (imageFromCamera ? 1 : 0)
+            };
+            Camera.getPicture(options).then((imageData)=>{
+                // imageData is either a base64 encoded string or a file URI
+            // If it's base64:
+            
+             let base64Image = "data:image/jpeg;base64," + imageData;
+
+            this.image = base64Image;
+
+        }, (err) => {
+            console.log(err);
+            alert(err);
+        });
+            }
+        }
+        
+        
+    }
+    
 
 
-}
