29efacf06cc5aa9a5dd237c5e321adbb090d8a64
aamir-shah <syedaamir974@gmail.com>
2016-10-14 16:53:26 +0500

    no need

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,22 +1,31 @@
-var ngTemplate = require('./../node_modules/@ionic/app-scripts/dist/plugins/ng-template').ngTemplate;
 var nodeResolve = require('rollup-plugin-node-resolve');
 var commonjs = require('rollup-plugin-commonjs');
+var globals = require('rollup-plugin-node-globals');
+var builtins = require('rollup-plugin-node-builtins');
+var json = require('rollup-plugin-json');
+
 
 // https://github.com/rollup/rollup/wiki/JavaScript-API
 
-module.exports = {
+var rollupConfig = {
   /**
    * entry: The bundle's starting point. This file will
    * be included, along with the minimum necessary code
    * from its dependencies
    */
-  entry: './.tmp/app/main.dev.js',
+  entry: '{{SRC}}/app/main.dev.ts',
 
   /**
    * sourceMap: If true, a separate sourcemap file will
    * be created.
    */
   sourceMap: true,
+
+
+  /**
+ * useStrict: this to avoid eval errors from preventing your build.
+ */
+  useStrict: false,
 
   /**
    * format: The format of the generated bundle
@@ -28,34 +37,44 @@ module.exports = {
    */
   dest: 'main.js',
 
-
-  // Add this to avoid Eval errors
-  useStrict: false,
-
   /**
    * plugins: Array of plugin objects, or a single plugin object.
    * See https://github.com/rollup/rollup/wiki/Plugins for more info.
    */
   plugins: [
-    ngTemplate(),
-    commonjs({
-      include: [
-        'node_modules/rxjs/**',
-        'node_modules/firebase/**',
-        'node_modules/angularfire2/**'
-      ],
-      namedExports: {
-        'node_modules/firebase/firebase.js': ['initializeApp', 'auth', 'database'],
-        'node_modules/angularfire2/node_modules/firebase/firebase-browser.js': ['initializeApp', 'auth', 'database']
+    builtins(),
+    commonjs(
+      {
+        include: [
+          'node_modules/rxjs/**', // firebase needs rxjs to avoid build errors
+          'node_modules/firebase/**', // here we're calling firebase.
+          'node_modules/angularfire2/**' // here we're calling angularfire2.
+        ],
+        namedExports: {
+          'node_modules/firebase/firebase.js': ['initializeApp', 'auth', 'database'],
+          'node_modules/angularfire2/node_modules/firebase/firebase-browser.js': ['initializeApp', 'auth', 'database']
+        }
       }
-    }),
+    ),
     nodeResolve({
       module: true,
       jsnext: true,
       main: true,
       browser: true,
       extensions: ['.js']
-    })
+    }),
+    globals(),
+    json()
   ]
 
 };
+
+
+if (process.env.IONIC_ENV == 'prod') {
+  // production mode
+  rollupConfig.entry = '{{TMP}}/app/main.prod.ts';
+  rollupConfig.sourceMap = false;
+}
+
+
+module.exports = rollupConfig;
